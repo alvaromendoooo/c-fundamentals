a0c07d3f41cb4d7f19444e89380e3e88dab2a435
alvaromendoooo <alvaromendo2004@gmail.com>
2026-09-03 12:35:31 +0200

    done

--- main.c
+++ main.c
@@ -5,10 +5,17 @@ int main(void) {
     //printf("Hello, C!\n");
     
     // Test 02
-    int a, b;
+    /*int a, b;
     
     scanf("%d %d", &a, &b);
-    printf("%d\n", a + b);
-  
+    printf("%d\n", a + b);*/
+    
+    // Test 03
+    int w, h;
+    
+    scanf("%d %d", &w, &h);
+    printf("%d\n", w * h);
+
+
     return 0;
 }
