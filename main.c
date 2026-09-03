c626b439cf434fcda955e23f0d6e55ee09d585ca
alvaromendoooo <alvaromendo2004@gmail.com>
2026-09-03 12:56:00 +0200

    done

--- main.c
+++ main.c
@@ -11,10 +11,25 @@ int main(void) {
     printf("%d\n", a + b);*/
     
     // Test 03
-    int w, h;
+    /*int w, h;
     
     scanf("%d %d", &w, &h);
-    printf("%d\n", w * h);
+    printf("%d\n", w * h);*/
+
+    // Test 04
+    int n = 5;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    int best = arr[0];
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > best) {
+            best = arr[i];
+        }
+    }
+
+    printf("%d", best);
 
 
     return 0;
